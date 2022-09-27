65b5b656dd4f783ce1948383fc942e56ce2a7208
Ziz Ahmed <azizazu0934@gmail.com>
2022-09-27 11:06:02 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Module to write text to a file"""
+"""
+Contains the function "wrtie_file"
+"""
 
 
 def write_file(filename="", text=""):
-    """Write `text` to `filename`
-    Args:
-        filename (str): file to write to
-        text (str): text to write into `filename`
-    Returns: number of characters written
-    """
-    with open(filename, 'w', encoding='utf8') as f:
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
