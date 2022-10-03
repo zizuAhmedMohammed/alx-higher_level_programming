d9f82932e010ca5a13d78a6ee49412bc01f60b3f
Ziz Ahmed <azizazu0934@gmail.com>
2022-10-03 11:23:21 -0700

    Base and init file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains the "Base" class
+"""
+
+import csv
+
+
+class Base:
+    """A base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize the base class"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
