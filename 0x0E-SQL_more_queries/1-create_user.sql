f481d9d32b2f2ca734e374751651279d6bf38913
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-02 13:27:16 -0700

    Task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
---A script that creates the MySQL server user user_0d_1
---user_0d_1 should have all privileges on your MySQL server
---The user_0d_1 password should be set to user_0d_1_pwd
---If the user user_0d_1 already exists, your script should not fail
-
+-- Creates a MySQL Server user `user_0d_1`
+-- and sets a temporary password `user_0d_1_pwd`.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
