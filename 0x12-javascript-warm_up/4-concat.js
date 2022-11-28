0a8141863d6dd7be28eb7e104a9a9bb41c48a858
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-28 13:09:18 -0800

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+if (process.argv.length === 4) console.log(process.argv[2] + ' is ' + process.argv[3]);
+else if (process.argv.length === 3) console.log(process.argv[2] + ' is ' + 'undefined');
+else console.log('undefined' + ' is ' + 'undefined');
