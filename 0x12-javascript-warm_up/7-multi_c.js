53d83da1588de24473bdd8e7bfe25a4d7731a94a
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-28 15:16:40 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const x = process.argv[2];
 
-if (!parseInt(x)) console.log('Missing number of occurences');
-else {
-  for (let i = 1; i <= x; i++)
+if (!parseInt(x)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 1; i <= x; i++) {
     console.log('C is fun');
+  }
 }
