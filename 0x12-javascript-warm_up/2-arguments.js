8a6ec6998febf816a06c38edfb005b33d67159f7
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-28 12:10:56 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
 if (process.argv.length < 3) console.log('No argument');
 else if (process.argv.length === 3) console.log('Argument found');
 else console.log('Arguments found');
