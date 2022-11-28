4dd6814daf560e9e640e5ca8d86e8f836ab0c976
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-28 11:39:17 -0800

    Tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-#!/bin/usr/node
+#!/usr/bin/node
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function printScript(arg) {
+  if (arguments.length < 1) console.log('No argument');
+  else if (arguments.length === 1) console.log('Argument found');
+  else console.log('Arguments found');
+  }
