ae7f935f4b58e2a3d0f8d0e7e9a4e48b9b622454
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-29 13:13:15 -0800

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w >= 0 && h >= 0) {
+    if (typeof w === 'number' && w >= 0 && typeof h === 'number' && h >= 0) {
     this.w = w;
     this.h = h;
     }
