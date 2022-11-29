4ed30bb511cb4baf2188d28ba3c16c49fa962490
Ziz Ahmed <azizazu0934@gmail.com>
2022-11-29 13:16:24 -0800

    2-rectangle.js modified

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (typeof w === 'number' && w >= 0 && typeof h === 'number' && h >= 0) {
-    this.w = w;
-    this.h = h;
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
     }
   }
 }
