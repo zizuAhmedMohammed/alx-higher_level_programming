c103977e58d6b9609cc47e8f85c75d2af0d0a349
Ziz Ahmed <azizazu0934@gmail.com>
2022-12-04 11:09:29 -0800

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,13 +12,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    with db.cursor() as db:
-        cur.execute(
-            "SELECT cities.id, cities.name, states.name \
-                FROM cities JOIN states ON cities.state_id \
-                = states.id ORDER BY cities.id ASC")
-        rows = db.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+    cur = db.cursor()
+    cur.execute("SELECT cities.`name` FROM cities "
+                "JOIN states ON cities.`state_id` = states.`id` "
+                "WHERE states.`name` = %s "
+                "COLLATE 'latin1_general_cs' "
+                "ORDER BY cities.`id` ASC", (sys.argv[4],))
+    print(', '.join(map(lambda x: x[0], cur.fetchall()))
