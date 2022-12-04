3edb00fafe048fe7276f70d0b26e47f00e01f3c7
Ziz Ahmed <azizazu0934@gmail.com>
2022-12-04 09:47:56 -0800

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script to display all rows of states table
+where name matches argument
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states \
+        WHERE name LIKE BINARY %(name) ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
