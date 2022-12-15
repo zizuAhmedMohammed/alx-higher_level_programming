e76f2c7e425eace455ae4dbd579bbf30fcb8e49b
Abdulaziz Ahmed <azizazu0934@gmail.com>
2022-12-15 13:03:54 -0800

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# you can use the -H command-line option and
-# pass the header name and value in "Key: Value" format.
+# you can use the -H command-line option and pass the header name and value in "Key: Value" format.
 curl -sH "X-School-User-Id: 98" "$1"
